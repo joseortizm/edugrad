0bbecdf934738a8963b148bc2cdaf7bc5c7a22e9
Jose <pepeisa.ortiz@gmail.com>
2024-04-03 23:09:00 -0500

    init tensor

--- edugrad/tensor.py
+++ edugrad/tensor.py
@@ -1,7 +1,13 @@
+import numpy as np
 
-
-
-
-
-
-
+class Tensor:
+    def __init__(self, data):
+        if type(data) != np.ndarray:
+            print("Error: constructing tensor with ", data)
+            assert(False)
+        self.data = data
+        self.grad = None
+        self._ctx = None
+    
+    def __str__(self):
+        return f"Tensor {self.data} with grad {self.grad}"
---
+++ test.py
@@ -0,0 +1,25 @@
+import numpy as np
+from edugrad.tensor import Tensor
+
+
+x_init_1 = np.random.randn(1,3) #float64
+x_init_2 = np.random.randn(1,3).astype(np.float32) #float32
+
+print(x_init_1, type(x_init_1))
+print(x_init_2, type(x_init_2))
+
+def checkFloat(number):
+    if type(number) == np.float32:
+        print("El número es float32")
+    elif type(number) == np.float64:
+        print("El número es float64")
+    else:
+        print("El número no es ni float32 ni float64")
+
+
+checkFloat(x_init_1[0][0])
+
+x_init = np.random.randn(1,3).astype(np.float32)
+print("value of x_init:", x_init)
+x = Tensor(x_init) 
+print(x)
