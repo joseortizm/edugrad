291586e0b5d19e2016d9864b69e4f12386a40c53
Jose <pepeisa.ortiz@gmail.com>
2024-04-05 01:35:25 -0500

    check variables in classes

--- edugrad/tensor.py
+++ edugrad/tensor.py
@@ -1,4 +1,5 @@
 import numpy as np
+from functools import partialmethod
 
 class Tensor:
     def __init__(self, data):
@@ -11,3 +12,58 @@ class Tensor:
     
     def __str__(self):
         return f"Tensor {self.data} with grad {self.grad}"
+
+class Context:
+    def __init__(self, arg, *tensors):
+        self.arg = arg
+        print("self.arg<->arg:", arg)
+        self.parents = tensors
+        print("self.parents<->tensor:", tensors)
+        self.saved_tensors = []
+    
+    def save_for_backward(self, *x):
+        self.saved_tensors.extend(x)
+
+
+class Function:
+    def apply(self, arg, *x):
+        ctx = Context(arg, self, *x)
+        print("ctx:", ctx)
+        ret = Tensor(arg.forward(ctx, self.data, *[t.data for t in x]))
+        print("x of Function:", x)
+        print("self.data of Function:", self.data)
+        print("ret:", ret)
+        ret._ctx = ctx
+        return ret
+
+def register(name, fxn):
+    setattr(Tensor, name, partialmethod(fxn.apply, fxn))
+    
+class Dot(Function):
+    @staticmethod
+    def forward(ctx, input, weight):
+      ctx.save_for_backward(input, weight)
+      print("input is", input)
+      print("input.dot(",weight,") is :", input.dot(weight))
+      return input.dot(weight)
+   
+    @staticmethod
+    def backward(ctx, grad_output):
+      input, weight = ctx.saved_tensors
+      print("input:", input)
+      print("weight:", weight)
+      grad_input = grad_output.dot(weight.T)
+      grad_weight = grad_output.T.dot(input).T
+      print("grad_input:", grad_input)
+      print("grad_weight:", grad_weight)
+      return grad_input, grad_weight
+
+register('dot', Dot)
+
+
+
+
+
+
+
+
--- test.py
+++ test.py
@@ -2,11 +2,11 @@ import numpy as np
 from edugrad.tensor import Tensor
 
 
-x_init_1 = np.random.randn(1,3) #float64
-x_init_2 = np.random.randn(1,3).astype(np.float32) #float32
-
-print(x_init_1, type(x_init_1))
-print(x_init_2, type(x_init_2))
+#x_init_1 = np.random.randn(1,3) #float64
+#x_init_2 = np.random.randn(1,3).astype(np.float32) #float32
+#
+#print(x_init_1, type(x_init_1))
+#print(x_init_2, type(x_init_2))
 
 def checkFloat(number):
     if type(number) == np.float32:
@@ -17,9 +17,18 @@ def checkFloat(number):
         print("El número no es ni float32 ni float64")
 
 
-checkFloat(x_init_1[0][0])
+#checkFloat(x_init_1[0][0])
 
 x_init = np.random.randn(1,3).astype(np.float32)
+
 print("value of x_init:", x_init)
 x = Tensor(x_init) 
-print(x)
+print("x value is:", x)
+
+W_init = np.random.randn(3,3).astype(np.float32)
+W = Tensor(W_init)
+print("W value is:", W)
+
+out = x.dot(W)
+print("out value is:", out)
+
