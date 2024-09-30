6754724ca10d203a490af14a21b994e7730b3579
Jose <pepeisa.ortiz@gmail.com>
2024-09-29 21:29:07 -0500

    CrossEntropyLoss

--- mnist.py
+++ mnist.py
@@ -37,12 +37,13 @@ test_labels_path = '../datasets/MNIST/raw/t10k-labels-idx1-ubyte'
 X_train = read_images(train_images_path)
 y_train = read_labels(train_labels_path)
 X_test = read_images(test_images_path)
-y_test = read_labels(test_labels_path)
+y_test = read_labels(test_labels_path) #usado para accuracy
 
-###Check imagenes###
+###Check dataset###
 #print("X_train[1]:")
 #print(X_train[1])
 #print("len(X_train[1]):", len(X_train[1])) #784 = 28*28
+#print("X_train.shape:", X_train.shape) #a mi ay viene con X_train.shape: (60000, 784) pero en colab no x eso usaron el reshape. ver si yo en verdad necesito en mi funcion norm_...
 #print("X_train[1].shape:", X_train[1].shape) #(784,)
 #print("X_train.shape:", X_train.shape) #(60000, 784)
 #print("len(X_train):", len(X_train)) #60000
@@ -50,109 +51,96 @@ y_test = read_labels(test_labels_path)
 #print("X_train.dtype:", X_train.dtype) #uint8
 #print("len(y_train):", len(y_train)) #60000
 #print("len(X_test):", len(X_test)) #10000
-#print("len(y_test):", len(y_test)) #10000
-###
-
+#print("y_test:", y_test[0]) #7
+#print("len(y_test):", len(y_test[0])) #10000
+##################
 
 ###Graficar un digito###
 ###
-def plot_number(image):
+def plot_number(image, target):
   plt.figure(figsize=(5,5))
+  target_string = str(target)
+  plt.title(target_string)
   plt.imshow(image.squeeze(), cmap=plt.get_cmap('gray')) #puede recibir imagenes en  uint8 o float32
   plt.axis('off')
   plt.show()
-
-#Picture Dataset Train:
 #randomIndex = random.randint(0, 59000)
 #digit = X_train[randomIndex].reshape(28,28)
-#plot_number(digit)
-#print(y_train[randomIndex])
+#label = y_train[randomIndex]
+#plot_number(digit, label)
 ###
 
-###Convertir tipo uint8 a float32###
-###
-#x_train = X_train.reshape(60000, -1).astype(np.float32)
-#print("type(x_train):", type(x_train)) #numpy.ndarray
-#print("x_train.dtype:", x_train.dtype) #float32
-#print("x_train.shape:", x_train.shape) #(60000, 784)
-#print("x_train[1]:")
-#print(x_train[1])
-#print(x_train[1].shape) #(784,)
-###
+def norm_reshape(xTrain, xTest):
+  train_images = np.asarray(xTrain, dtype=np.float32) / 255.0
+  test_images = np.asarray(xTest, dtype=np.float32) / 255.0
+  #train_images = train_images.reshape(60000,784)
+  #test_images = test_images.reshape(10000,784)
+  
+  #yTrain = keras.utils.to_categorical(yTrain) ###usado en entrenamiento [0 0 0 1 0 0 0 ...] revisar como lograr esto sin keras (antes entender xq lo uso)
+  return train_images, test_images 
+
+trainImages, testImages = norm_reshape(X_train, X_test) 
+#print(trainImages[0].shape) #(784,)
+
+#TODO: class CrossEntropyLoss
+#https://machinelearningmastery.com/cross-entropy-for-machine-learning/
+#search others urls
+
+##Logits:
+Wb = np.random.randn(784,10)# new initialized weights for gradient descent
+#(784, 10) where 
+#Wb = [[w11, w12, w13, ..., w1 10],
+#     [w21, w22, w23, ..., w2 10],
+#     ...,
+#     [w7841, w7842, w7843, ..., w78410]]
+
+batch_size = 32
+n_imgs = trainImages.shape[0] #60000
+ri = np.random.permutation(n_imgs)[:batch_size] #[first 32 random number between 0 - 59000] 
+
+'''
+print(ri) #[.....]
+print(ri[0])
+print(trainImages[ri[0]])
+print("suma matmul 1:")
+#print(np.matmul(trainImages[ri[0]], Wb))
+suma1 = sum(np.matmul(trainImages[ri[0]], Wb))
+print(suma1)
+'''
+
+'''
+out = np.matmul(trainImages[ri], Wb)
+print(out.shape) #32,10
+print(out)#10 elements
+print("suma matmul 2:")
+print(sum(out[0]))
+'''
+X = trainImages[ri] #revisar si OK
+logits = np.matmul(trainImages[ri], Wb)
+
+## expo logits:
+exp_logits = np.exp(logits)
+
+##softmax:
+softmax_probs = exp_logits / np.sum(exp_logits, axis=1, keepdims=True)
+#np.exp(np.matmul(X,W)) / np.sum(np.exp(np.matmul(X,W)),axis=1)[:, None] #original
+
+## Calcula la pérdida de entropía cruzada
+Y = y_train[ri].reshape(-1,1) #revisar si OK
+loss_terms = Y * np.log(softmax_probs)
+#Y*np.log(np.exp(np.matmul(X,W)) / np.sum(np.exp(np.matmul(X,W)),axis=1)[:, None]),axis = 1)
+
+## Suma para cada muestra
+loss = -np.sum(loss_terms, axis=1) 
+#np.sum(Y*np.log(np.exp(np.matmul(X,W)) / np.sum(np.exp(np.matmul(X,W)),axis=1)[:, None]),axis = 1)
+
+## Promedio de la pérdida
+average_loss = -(1 / X.shape[0]) * np.sum(loss) 
+#-(1/X.shape[0])*np.sum(np.sum(Y*np.log(np.exp(np.matmul(X,W)) / np.sum(np.exp(np.matmul(X,W)),axis=1)[:, None]),axis = 1))
+print(average_loss)
 
 
-###Ejemplos de funciones linear, relu, sigmoid###
-def exam_func():
-  x_train = X_train.reshape(60000, -1).astype(np.float32)
-  x = x_train[1]/255
-  x = Tensor(x)
-  #print("x")
-  #print(x)
 
-  linear = nn.Linear(784, 64)
-  print(linear) 
-  output_linear = linear(x)
-  print("output_linear:")
-  print(output_linear) 
-  print(output_linear.shape()) #64
-
-  #sigmoid = nn.Sigmoid()
-  #print("Sigmoid:")
-  #print(sigmoid(output_linear))
-  #
-  #relu = nn.ReLU()
-  #print("ReLU eduGrad:")
-  #print(relu(output_linear))
-  print("Pytorch:")
-  import torch
-  xT = x_train[1]/255
-  xT = torch.tensor(xT)
-  #print("xT")
-  #print(xT)
-
-  linearT = torch.nn.Linear(784, 64)
-  print(linearT) 
-  output_linearT = linearT(xT)
-  print("output_linearT:")
-  print(output_linearT) 
-  print(output_linearT.shape) 
-
-  #sigmoidT = torch.nn.Sigmoid()
-  #print("SigmoidT:")
-  #print(sigmoidT(output_linearT))
-  #
-  #reluT = torch.nn.ReLU()
-  #print("ReLU Pytorch:")
-  #print(reluT(output_linearT))
-
-#exam_func()
-###
-
-x_train = X_train.reshape(60000, -1).astype(np.float32)
-x = x_train[0]/255
-x = Tensor(x)
-print(x)
-print(x.shape())
-
-class Net():
-  def __init__(self):
-    self.fc1 = nn.Linear(28*28, 512)
-    self.fc2 = nn.Linear(512, 10)  
-
-  def forward(self, x):
-    relu1 = nn.ReLU()
-    x = relu1(self.fc1(x))
-    print("otro:", x)
-    x = self.fc2(x)
-    print("otro:", x.data)
-    return x
-    
-model = Net()
-output = model.forward(x)
-print(output)
-
-soft = nn.Softmax()
-pred_probab = soft(output)
-print(pred_probab.shape())
-
-
+#class CrossEntropyLoss():
+#  def __init__():
+#    pass
